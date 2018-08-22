807db948cb08fd7af0748cc59a4acd98d7ff3921
Andre Pires <andrecabralpires@gmail.com>
2018-08-22 11:44:31 +0100

    rails end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    member do
+      get 'chef', to: "restaurants#chef"
+    end
+    collection do
+    get 'top', to: "restaurants#top"
+    end
+    resources :reviews,only: [:new, :create]
+  end
+  resources :reviews, except: [:new,:create,:index]
 end
